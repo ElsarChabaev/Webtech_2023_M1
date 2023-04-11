92d025d2a22f7a7e747106d48bd6ba160101f912
Elsar Chabaev <s0561238@htw-berlin.de>
2023-04-11 13:25:54 +0200

    Erster Testcommit

---
+++ src/main/java/Webtech2023/icomify/IcomifyApplication.java
@@ -0,0 +1,13 @@
+package Webtech2023.icomify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcomifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcomifyApplication.class, args);
+	}
+
+}
